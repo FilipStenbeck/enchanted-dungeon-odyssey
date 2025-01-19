fa8b71f92e6a7a14976cc427a5c8afde6e15d95d
Filip Stenbeck <filip.stenbeck@mentimeter.com>
2025-01-19 14:36:07 +0100

    set health based on class

--- src/utils/charachterGenerator.ts
+++ src/utils/charachterGenerator.ts
@@ -1,23 +1,35 @@
 import { Character } from "@/types/character";
 import { CharacterClass } from "@/types/character";
+import { get } from "http";
 
 export function generateCharacter() { 
-    return <Character> {
-        class: randomizeClass(),
-        health: 100,
-        maxHealth: 100,
-        attack: 15,
-        defence: 10,
-        weapon: 'Short Sword',
-        armor: 'Leather armor',
-        inventory: ['Health Potion', 'Torch', 'Rope'],
-        specialAction: 'Power Attack: Deal double damage on your next attack'
-      };
+    return randomizeClass(); 
 }
-
 
 
 const randomizeClass = () => {
   const classes = ['wizard', 'fighter', 'thief'];
-  return classes[Math.floor(Math.random() * classes.length)];
+  const me =  classes[Math.floor(Math.random() * classes.length)];
+  return <Character> {
+    class: me,
+    health: getHealth(me),
+    maxHealth: getHealth(me),
+    attack: 7,
+    defence: 10,
+    weapon: 'Short Sword',
+    armor: 'Leather armor',
+    inventory: ['Health Potion', 'Torch', 'Rope'],
+    specialAction: 'Power Attack: Deal double damage on your next attack'
+  };
+}
+
+const getHealth = (me) => {
+  switch (me) {
+    case 'wizard':
+      return 50;
+    case  'fighter':
+      return 100;
+    case 'thief':
+      return 75;
+  }
 }
