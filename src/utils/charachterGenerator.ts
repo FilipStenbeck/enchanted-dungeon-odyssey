bcb8365f67e3cab047c62fe1261060c00e82a359
Filip Stenbeck <filip.stenbeck@mentimeter.com>
2025-01-19 14:27:57 +0100

    Randomize starting class

--- src/utils/charachterGenerator.ts
+++ src/utils/charachterGenerator.ts
@@ -1,7 +1,9 @@
 import { Character } from "@/types/character";
+import { CharacterClass } from "@/types/character";
 
 export function generateCharacter() { 
     return <Character> {
+        class: randomizeClass(),
         health: 100,
         maxHealth: 100,
         attack: 15,
@@ -11,4 +13,11 @@ export function generateCharacter() {
         inventory: ['Health Potion', 'Torch', 'Rope'],
         specialAction: 'Power Attack: Deal double damage on your next attack'
       };
+}
+
+
+
+const randomizeClass = () => {
+  const classes = ['wizard', 'fighter', 'thief'];
+  return classes[Math.floor(Math.random() * classes.length)];
 }
