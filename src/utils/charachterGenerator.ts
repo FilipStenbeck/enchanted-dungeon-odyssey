da38aadf56b8289e728a301e06176554a40ed001
Filip Stenbeck <filip.stenbeck@mentimeter.com>
2025-01-19 16:18:19 +0100

    added specific attributes per class

--- src/utils/charachterGenerator.ts
+++ src/utils/charachterGenerator.ts
@@ -14,12 +14,12 @@ const randomizeClass = () => {
     class: me,
     health: getHealth(me),
     maxHealth: getHealth(me),
-    attack: 7,
-    defence: 10,
-    weapon: 'Short Sword',
-    armor: 'Leather armor',
+    attack: getAttack(me),
+    defence: getDefence(me),
+    weapon: getWeapon(me),
+    armor: getArmor(me),
     inventory: ['Health Potion', 'Torch', 'Rope'],
-    specialAction: 'Power Attack: Deal double damage on your next attack'
+    specialAction: getSpecialAction(me),
   };
 }
 
@@ -31,5 +31,60 @@ const getHealth = (me) => {
       return 100;
     case 'thief':
       return 75;
+  }
+}
+
+const getAttack = (me) => {
+  switch (me) {
+    case 'wizard':
+      return 12;
+    case  'fighter':
+      return 10;
+    case 'thief':
+      return 7;
+  }
+}
+
+const getDefence = (me) => {
+  switch (me) {
+    case 'wizard':
+      return 10;
+    case  'fighter':
+      return 15;
+    case 'thief':
+      return 12;
+  }
+}
+
+const getWeapon = (me) => {
+  switch (me) {
+    case 'wizard':
+      return 'Staff';
+    case  'fighter':
+      return 'Short Sword';
+    case 'thief':
+      return 'Dagger';
+  }
+}
+
+const getArmor = (me) => { 
+  switch (me) {
+    case 'wizard':
+      return 'Robes';
+    case  'fighter':
+      return 'Chainmail';
+    case 'thief':
+      return 'Leather armor';
+  } 
+}
+
+const getSpecialAction = (me) => {
+  switch (me) {
+    case 'wizard':
+      return 'Fireball: Deal 20 damage to all enemies';
+    case  'fighter':
+      return 'Power Attack: Deal double damage on your next attack';
+    case 'thief':
+      return 'Sneak: Automatically escape from combat';
   }
 }
